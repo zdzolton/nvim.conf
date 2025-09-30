b8d6eee2f4bcb901e5e3704064b4575f5d8c420c
Zach Zolton <zachary.zolton@gmail.com>
2025-09-29 20:45:43 -0400

    Fix gitsigns setup

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -2,15 +2,29 @@
 -- https://github.com/lewis6991/gitsigns.nvim
 
 -- See `:help gitsigns` to understand what the configuration keys do
-return { 
-  'lewis6991/gitsigns.nvim',
-  opts = {
-    signs = {
-      add = { text = '+' },
-      change = { text = '~' },
-      delete = { text = '_' },
-      topdelete = { text = '‾' },
-      changedelete = { text = '~' },
-    },
-  },
+return {
+	"lewis6991/gitsigns.nvim",
+	event = "BufReadPre", -- Load the plugin before reading a buffer
+	config = function()
+		require("gitsigns").setup({
+			-- Your Gitsigns configuration options here
+			signs = {
+				add = { text = "+" },
+				change = { text = "~" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+			},
+			signcolumn = true, -- Show the sign column
+			numhl = true, -- Highlight line numbers
+			linehl = false, -- Highlight the entire line
+			word_diff = false, -- Enable word-level diffs
+			watch_gitdir = {
+				follow_files = true,
+			},
+			auto_attach = true,
+			current_line_blame = false, -- Display blame info on current line
+			-- More options can be found in :help gitsigns-setup
+		})
+	end,
 }
