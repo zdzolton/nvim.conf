a013d1c4d980be437a1393cded98c7722aaacc5d
Zach Zolton <zach.zolton@sproutsocial.com>
2025-12-04 14:42:05 -0500

    Merge basic Java LSP support (#1)
    * Control nvim-java initialization
    
    * Make my Java LSP setup hack more palatable
    
    * Prevent multiple JDTLS instances
    
    * Move Java LSP autocommand
    
    * Disable inlayHints for JDTLS

---
+++ lua/plugins/nvim-java.lua
@@ -0,0 +1,61 @@
+return {
+	"nvim-java/nvim-java",
+	dependencies = {
+		"nvim-java/lua-async-await",
+		"nvim-java/nvim-java-core",
+		"nvim-java/nvim-java-test",
+		"nvim-java/nvim-java-dap",
+		"MunifTanjim/nui.nvim",
+		"neovim/nvim-lspconfig",
+		"mfussenegger/nvim-dap",
+	},
+	config = function()
+		-- Delay Java initialization to avoid errors for unrelated file types:
+		-- https://github.com/nvim-java/nvim-java/issues/427
+		-- https://github.com/nvim-java/nvim-java/issues/293
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "java",
+			once = true,
+			callback = function()
+				-- used to enable autocompletion (assign to every lsp server config)
+				local capabilities = require("blink.cmp").get_lsp_capabilities()
+				-- enable snippet
+				capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+				-- nvim-java handles JDTLS configuration automatically.
+				-- It will call lspconfig.jdtls.setup() internally.
+				require("java").setup({
+					jdk = {
+						auto_install = false,
+					},
+					lsp = {
+						capabilities = capabilities,
+						settings = {
+							java = {
+								inlayHints = {
+									parameterNames = {
+										enabled = false,
+									},
+								},
+							},
+						},
+						on_attach = function(client, bufnr)
+							if client.server_capabilities.inlayHintProvider then
+								vim.lsp.inlay_hint.enable(false, { bufnr = bufnr })
+							end
+						end,
+					},
+					java_test = {
+						enable = false,
+					},
+					java_debug_adapter = {
+						enable = false,
+					},
+					spring_boot_tools = {
+						enable = false,
+					},
+				})
+			end,
+		})
+	end,
+}
